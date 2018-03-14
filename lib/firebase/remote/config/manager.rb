4df77c4fcdd399403e519103ebe555941525ac0c
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2018-03-15 01:58:41 +0900

    Initial commit

---
+++ lib/firebase/remote/config/manager.rb
@@ -0,0 +1,11 @@
+require "firebase/remote/config/manager/version"
+
+module Firebase
+  module Remote
+    module Config
+      module Manager
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/firebase/remote/config/manager/version.rb
@@ -0,0 +1,9 @@
+module Firebase
+  module Remote
+    module Config
+      module Manager
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
