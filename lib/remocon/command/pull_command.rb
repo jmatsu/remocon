41a25f48daa05805d31bd86a223da74a5bfcc1e8
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-11 16:18:58 +0900

    Extracted diff methods

--- lib/remocon/command/pull_command.rb
+++ lib/remocon/command/pull_command.rb
@@ -58,52 +58,13 @@ module Remocon
         parameters = raw_hash[:parameters] || {}
 
         if config.merge? && File.exist?(config.parameters_file_path) && File.exist?(config.parameters_file_path)
-          added_condition_keys = conditions.map { |c| c[:name] } - left.raw_conditions.map { |c| c[:name] }
-          removed_condition_keys = left.raw_conditions.map { |c| c[:name] } - conditions.map { |c| c[:name] }
 
-          added_conditions = added_condition_keys.each_with_object([]) do |k, acc|
-            acc.push(conditions.find { |c| c[:name] == k })
-          end
+          require 'pry'
+          unchanged_conditions, added_conditions, changed_conditions, = conditions_diff(left.raw_conditions, conditions)
+          unchanged_parameters, added_parameters, changed_parameters, = parameters_diff(left.raw_parameters, parameters)
 
-          unchanged_name_conditions = (conditions.map { |c| c[:name] } & left.raw_conditions.map { |c| c[:name] }).each_with_object([]) do |k, acc|
-            new = conditions.find { |c| c[:name] == k }
-            old = left.raw_conditions.find { |c| c[:name] == k }
-
-            if old == new
-              acc.push(old)
-            else
-              acc.push(new)
-            end
-          end
-
-          removed_condition_keys.each do |k|
-            left.raw_conditions.delete_if {|c| c[:name] == k }
-          end
-
-          added_parameter_keys = parameters.keys - left.raw_parameters.keys
-          removed_parameter_keys = left.raw_parameters.keys - parameters.keys
-
-          added_parameters = added_parameter_keys.each_with_object({}) do |k, acc|
-            acc[k] = parameters[k]
-          end
-
-          unchanged_name_parameters = (parameters.keys & left.raw_parameters.keys).each_with_object({}) do |k, acc|
-            new = parameters[k]
-            old = left.raw_parameters[k]
-
-            if old == new
-              acc[k] = old
-            else
-              acc[k] = new
-            end
-          end
-
-          removed_parameter_keys.each do |k|
-            left.raw_parameters.delete(k)
-          end
-
-          conditions_yaml = JSON.parse(sort_conditions(unchanged_name_conditions + Remocon::ConditionFileDumper.new(added_conditions).dump).to_json).to_yaml
-          parameters_yaml = JSON.parse(sort_parameters(unchanged_name_parameters.merge(Remocon::ParameterFileDumper.new(added_parameters).dump)).to_json).to_yaml
+          conditions_yaml = JSON.parse(sort_conditions(unchanged_conditions + added_conditions + changed_conditions).to_json).to_yaml
+          parameters_yaml = JSON.parse(sort_parameters(unchanged_parameters.merge(added_parameters).merge(changed_parameters)).to_json).to_yaml
         else
           conditions_yaml = JSON.parse(Remocon::ConditionFileDumper.new(sort_conditions(conditions)).dump.to_json).to_yaml
           parameters_yaml = JSON.parse(Remocon::ParameterFileDumper.new(sort_parameters(parameters)).dump.to_json).to_yaml
@@ -138,6 +99,71 @@ module Remocon
         end
 
         [raw_json, etag]
+      end
+
+      def conditions_diff(left, right)
+        left_names = left.map{ |c| c[:name] }
+        right_names = right.map{ |c| c[:name] }
+
+        added_names = right_names - left_names
+        removed_names = left_names - right_names
+
+        added = added_names.each_with_object([]) do |k, acc|
+          acc.push(right.find { |c| c[:name] == k })
+        end
+
+        changed = []
+        unchanged = []
+
+        (right_names & left_names).each do |k|
+          old = left.find { |c| c[:name] == k }
+          new = Remocon::ConditionFileDumper.new(right.find { |c| c[:name] == k }).dump
+
+          if old == new
+            unchanged.push(old)
+          else
+            changed.push(new)
+          end
+        end
+
+        removed = []
+
+        removed_names.each do |k|
+          removed.push(left.find {|c| c[:name] == k })
+        end
+
+        [unchanged, added, changed, removed]
+      end
+
+      def parameters_diff(left, right)
+        added_keys = right.keys - left.keys
+        removed_keys = left.keys - right.keys
+
+        added = added_keys.each_with_object({}) do |k, acc|
+          acc[k] = right[k]
+        end
+
+        changed = {}
+        unchanged = {}
+
+        (right.keys & left.keys).each do |k|
+          old = { k => left[k] }
+          new = Remocon::ParameterFileDumper.new({ k => right[k] }).dump
+
+          if old == new
+            unchanged.merge!(old)
+          else
+            changed.merge!(new)
+          end
+        end
+
+        removed = {}
+
+        removed_keys.each do |k|
+          removed[k] = left[k]
+        end
+
+        [unchanged, added, changed, removed]
       end
     end
   end
