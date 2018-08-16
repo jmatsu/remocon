100c54f364e17a610290ed8ba62881995499eb33
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-17 01:04:16 +0900

    Fixed conditional values sort

--- lib/remocon/sorter/parameter_sorter.rb
+++ lib/remocon/sorter/parameter_sorter.rb
@@ -9,12 +9,29 @@ module Remocon
         hash_arr = v.symbolize_keys.sort { |(a, _), (b, _)| PARAMETER_KEYS.index(a) <=> PARAMETER_KEYS.index(b) }
           .map do |k1, v1|
           {
-            k1 => k1.to_sym == :conditions ? sort_parameters(v1) : v1
+            k1 => k1.to_sym == :conditions ? sort_conditions(v1) : v1
           }
         end
 
         {
           k => hash_arr.each_with_object({}) { |hash, acc| acc.merge!(hash) }
+        }
+      end
+
+      arr.each_with_object({}) { |hash, acc| acc.merge!(hash) }.with_indifferent_access
+    end
+
+    def sort_conditions(conditions)
+      arr = conditions.map do |k, v|
+        hash_arr = v.symbolize_keys.sort { |(a, _), (b, _)| PARAMETER_KEYS.index(a) <=> PARAMETER_KEYS.index(b) }
+                       .map do |k1, v1|
+          {
+              k1 => v1
+          }
+        end
+
+        {
+            k => hash_arr.each_with_object({}) { |hash, acc| acc.merge!(hash) }
         }
       end
 
